ca8fcfd98aec712c4bc926c765922959a173df7f
Franco Veloci <fveloci@hotmail.com>
2018-05-08 20:53:11 -0300

    Codigo Java

---
+++ ProyectoDamas/src/proyectodamas/TableroDamas.java
@@ -0,0 +1,22 @@
+
+package proyectodamas;
+
+import java.awt.Button;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Label;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class TableroDamas extends Canvas  {
+    public TableroDamas() {
+		// Constructor. Create the buttons and lable. Listen for mouse
+		// clicks and for clicks on the buttons. Create the board and
+		// start the first game.
+		setBackground(Color.blue);
+		
+	}
+		
+}
