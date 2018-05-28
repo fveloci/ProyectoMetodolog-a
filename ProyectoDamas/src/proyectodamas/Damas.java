ce060fae14ae34e50108d338e6ebf565e4e5bbd9
Luciano <>
2018-05-28 00:10:21 -0300

    26-05-18. Implementacion Grafica

--- ProyectoDamas/src/proyectodamas/Damas.java
+++ ProyectoDamas/src/proyectodamas/Damas.java
@@ -5,34 +5,77 @@ import java.applet.*;
 
 import java.awt.Color;
 import java.awt.Font;
-
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.Date;
 
 
 public class Damas extends Applet {
    
-   public void init() {  
+   public void init() {  // con el metodo INIT sobreescrito desde applet , Inicializamos la ventana, que contendra el awt del juego. 
     
 		
                 
                
-		setSize(1024,1024);
-		setLayout(null); 
-                TableroDamas tablero = new TableroDamas(); // 
-                tablero.botonNuevoJuego.setBackground(Color.GREEN);
-                add(tablero);
-		add(tablero.botonNuevoJuego);
-
-		tablero.botonAbandona.setBackground(Color.RED);
-		add(tablero.botonAbandona);
-
-		tablero.mensaje.setForeground(Color.green);
-              
-		tablero.mensaje.setFont(new Font("Serif", Font.TRUETYPE_FONT, 32));
-		add(tablero.mensaje);
+		setSize(950,850); // DAMOS ANCHO Y ALTO  A LA VENTANA DEL APPLET
+		setLayout(null);  // SETEAMOS UN LAYOUT SIN DEFINIR PARA QUE INICIE
+                setBackground(Color.GREEN.darker());// LE DAMOS UN FONDO AL APPLET
+                Frame c = (Frame)this.getParent().getParent(); // DAMOS TITULO AL JUEGO
+                c.setTitle("DAMAS BETA 0.01");
+                TableroDamas tablero = new TableroDamas(); // INICIAMOS EL OBJETO TABLERO CON TODAS SUS REGLAS
                 
-		tablero.setBounds(30, 30, 645,645 ); 
-		tablero.botonNuevoJuego.setBounds(1100, 250, 200, 100);
-		tablero.botonAbandona.setBounds(1100, 400, 200, 100);
+                
+                // CREANDO TITULO DEL JUEGO MEDIANTE UN BOTON 
+                tablero.tituloJuego.setBackground(Color.BLACK);
+                add (tablero.tituloJuego);
+                tablero.tituloJuego.setForeground(Color.WHITE);
+                 
+                
+                // CREACION DEL PANEL 
+               
+                
+               tablero.panelDeJuego.setBounds(680,10,245,790); 
+               tablero.panelDeJuego.setBackground(Color.black);
+               
+               add (tablero.panelDeJuego);
+                
+                tablero.botonNuevoJuego.setBackground(Color.BLACK); // LE DAMOS COLOR AL COLOR DE BOTON NUEVO JUEGO
+                add(tablero);// AGREGAMOS EL OBJETO A INIT
+		add(tablero.botonNuevoJuego); // TAMBIEN AGREGAMOS EL BOTON PARA QUE APAREZCA 
+                tablero.botonNuevoJuego.setForeground(Color.GREEN);
+                // CREO LA CAJA PARA PONEER EL TIEMPO DE JUEGO :
+                tablero.CajaTiempo.setForeground(Color.white);
+		
+                tablero.botonAbandona.setBackground(Color.black); // DAMOS UN COLOR AL BOTO NUEVO JUEGO
+                tablero.botonAbandona.setForeground(Color.red);
+		add(tablero.botonAbandona); // AGREGAMOS EL BOTON AL INIT 
+                		
+                tablero.mensaje.setForeground(Color.green);
+		
+                tablero.mensaje.setFont(new Font("Serif", Font.ROMAN_BASELINE, 32));
+		add(tablero.mensaje);
+                add (tablero.CajaTiempo);
+                tablero.CajaTiempo.setBackground(Color.BLACK);
+                
+                
+                
+                
+                tablero.setBounds(20, 150, 645,645 );  // PROCEDEMOS A DAR LIMITES , Y LUGAR DE POSICION DE CADA OBJETO
+		tablero.botonNuevoJuego.setBounds(20, 90, 200, 50);
+		tablero.botonAbandona.setBounds(465, 90, 200, 50);
+		tablero.CajaTiempo.setBounds(250, 90, 200 , 50);
+                tablero.tituloJuego.setBounds(20,10, 645, 60);
+                
+                
+               
+                
+               
+                
+                                    
+                 }
+    
                 
 }
-  } 
+   
+   
